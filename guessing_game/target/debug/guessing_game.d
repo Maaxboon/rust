87500583164a9571551bfeeb53eaaef4c473f69b
Maaxboon <maaxwelokoth@gmail.com>
2025-07-05 07:08:10 +0300

    Removes deleted files

--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/home/maaxboon/repos/rust/guessing_game/target/debug/guessing_game: /home/maaxboon/repos/rust/guessing_game/src/main.rs
--- hello_world/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/maaxboon/repos/rust/hello_world/hello_cargo/target/debug/hello_cargo: /home/maaxboon/repos/rust/hello_world/hello_cargo/src/main.rs
