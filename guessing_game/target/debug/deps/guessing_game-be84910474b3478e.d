7233937e58d1d79ffc7320659dfe9f29124aed65
Maaxboon <maaxwelokoth@gmail.com>
2025-07-10 23:37:43 +0300

    Lints with cargo clippy

---
+++ guessing_game/target/debug/deps/guessing_game-be84910474b3478e.d
@@ -0,0 +1,9 @@
+/home/maaxboon/repos/rust_projects/guessing_game/target/debug/deps/libguessing_game-be84910474b3478e.rmeta: src/main.rs Cargo.toml
+
+/home/maaxboon/repos/rust_projects/guessing_game/target/debug/deps/guessing_game-be84910474b3478e.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
